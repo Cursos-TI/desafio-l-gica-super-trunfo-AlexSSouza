92a5a97a21a5298d9b08ff838f9518ee3616f22a
AlexSSouza <alexsilvasouza.asa@gmail.com>
2025-02-11 19:30:03 -0300

    melhoria

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -15,6 +15,7 @@ int main() {
     double area1, area2;
     double  pop1, pop2;
     double PIB1, PIB2;
+    int pontos; // Novo campo para armazenar a pontuação
     
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
@@ -26,21 +27,26 @@ int main() {
     // (Repita para cada propriedade)
     printf("Digite o Código de cada Cidade, Ex: A01 \n");
     scanf("%s", &Cod1);
+    printf("Digite o Código da segunda Cidade, Ex: A02 \n");
     scanf("%s", &Cod2);
-    printf("Digite o Nome da Cidade: \n");
+    printf("Digite o Nome de cada Cidade: \n");
     scanf("%s", &nome1);
     scanf("%s", &nome2);
-    printf("Digite a área da Cidade, Ex: 123.456\n");
+    printf("Digite a área da Cidade %s Ex: 123.456\n", nome1);
     scanf("%lf", &area1);
+    printf("Digite a área da Cidade %s Ex: 123.456\n", nome2);
     scanf("%lf", &area2);
-    printf("Digite o número da População Ex: 123456.78 \n");
+    printf("Digite o número da População da cidade %s Ex: 123456.78 \n", nome1);
     scanf("%lf", &pop1);
+    printf("Digite o número da População da cidade %s Ex: 123456.78 \n", nome2);
     scanf("%lf", &pop2);
-    printf("Digite o valor do PIB da Cidade, Ex: 98456.12\n");
+    printf("Digite o valor do PIB da Cidade %s, Ex: 98456.12\n", nome1);
     scanf("%lf", &PIB1);
+    printf("Digite o valor do PIB da Cidade %s, Ex: 98456.12\n", nome2);
     scanf("%lf", &PIB2);
-    printf("Digite a quantidade de Pontos Turísticos da Cidade\n");
+    printf("Digite a quantidade de Pontos Turísticos da Cidade %s\n", nome1);
     scanf("%d", &PointT1);
+    printf("Digite a quantidade de Pontos Turísticos da Cidade %s\n", nome2);
     scanf("%d", &PointT2);
 
     // Comparação de Cartas:
@@ -57,33 +63,37 @@ int main() {
     // Exibição dos Resultados:
     // Após realizar as comparações, exiba os resultados para o usuário.
     // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-    if (area1 > area2)
-    {
+    if (area1 > area2){
         printf("Cidade: %s tem maior população\n", nome1);
-    }else
-    {
+        Cod1 > pontos++;
+    }
+    else{
         printf("Cidade: %s tem maior população\n", nome2);
+        Cod2 >pontos++;
     }
-    if (pop1 > pop2)
-    {
+    if (pop1 > pop2){
         printf("Cidade %s tem maior população: %lf\n", nome1,pop1);
-    }else
-    {
+        Cod1 > pontos++;
+    }
+    else{
         printf("Cidade %s tem maior população: %lf\n", nome2,pop2);
+        Cod2 > pontos++;
     }
-    if (PIB1 > PIB2)
-    {
+    if (PIB1 > PIB2){
         printf("Cidade %s tem maior PIB: %lf\n", nome1,PIB1);
-    }else
-    {
-        printf("Cidade %s tem maior PIB: %lf\n", nome2,PIB2);
+        Cod1 > pontos++;
     }
-    if (PointT1 > PointT2)
-    {
+    else{
+        printf("Cidade %s tem maior PIB: %lf\n", nome2,PIB2);
+        Cod2 > pontos++;
+    }
+    if (PointT1 > PointT2){
         printf("Cidade %s tem maior quantidade de Pontos Turísticos: %d \n", nome1,PointT1);
-    }else
-    {
+        Cod1 > pontos++;
+    }
+    else{
         printf("Cidade %s Atem maior quantidade de Pontos Turísticos: %d \n", nome2,PointT2);
+        Cod2 > pontos++;
     }
     // Exemplo:
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
